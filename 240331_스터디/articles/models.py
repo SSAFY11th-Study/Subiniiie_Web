70e52e97d385b4a09653072c847b4c0790ae5391
박수빈 <subin57419@gmail.com>
2024-03-31 23:54:36 +0900

    240331_스터디

---
+++ 240331_스터디/articles/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model) :
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=20)
+    image = models.ImageField(blank=True)
+    image_description = models.TextField()
+    is_public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
